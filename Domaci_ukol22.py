ad50bd8c7774a83a70e5885c3cecd2dfe4da6016
EvaJed <eva.jedlicka@volny.cz>
2026-02-22 11:58:10 +0100

    Refactor variable name for request data

--- Domaci_ukol22.py
+++ Domaci_ukol22.py
@@ -3,8 +3,8 @@ nazev_firmy = input ("Zadej název firmy: ")
 url = "https://ares.gov.cz/ekonomicke-subjekty-v-be/rest/ekonomicke-subjekty/vyhledat"
 
 headers = {"accept":"application/json", "Content-Type":"application/json"} 
-data_pro_ares = {"obchodniJmeno":nazev_firmy}
-odpoved = requests.post(url, headers=headers, json=data_pro_ares)  
+data_pro_dotaz = {"obchodniJmeno":nazev_firmy}
+odpoved = requests.post(url, headers=headers, json=data_pro_dotaz)  
 vysledek = odpoved.json()
 seznam_firem = vysledek.get("ekonomickeSubjekty", [])                            
 print(f"Nalezeno firem: {len(seznam_firem)}")
@@ -12,4 +12,5 @@ print(f"Nalezeno firem: {len(seznam_firem)}")
 for firma in seznam_firem:
     nazev = firma.get("obchodniJmeno") 
     ico = firma.get("ico")       
+
     print(f"{nazev}, IČO:{ico}")                                                                          
